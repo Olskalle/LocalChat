019e14f978770d441eb4a13ed7f9cde529aee0cb
Olskalle <101509963+Olskalle@users.noreply.github.com>
2023-10-14 20:51:31 +0500

    broadcast change

--- ChatClient.cs
+++ ChatClient.cs
@@ -21,7 +21,8 @@ namespace LocalChat
 
         public ChatClient(bool startImmediate)
         {
-			brodcastAddress = IPAddress.Parse("235.5.5.11");
+			//brodcastAddress = IPAddress.Parse("235.5.5.11");
+			brodcastAddress = IPAddress.Parse("192.168.0.255");
 			client = new UdpClient(localPort);
 			if (startImmediate) this.Run();
 			isActive = startImmediate;
